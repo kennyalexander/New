13bf1a9fc5156134f849467282c2bf9115e46b13
Jo-gonzalezu <90359419+Jo-gonzalezu@users.noreply.github.com>
2021-11-25 01:53:05 -0300

    Inicio de proyecto

---
+++ Proyecto_Cooperativo/src/proyecto_cooperativo/Proyecto_Cooperativo.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_cooperativo;
+
+/**
+ *
+ * @author Aspire E3-431-2818
+ */
+public class Proyecto_Cooperativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO");
+        
+        
+        
+        /*Kenny agrega tu print bajo esta linea*/
+        
+        
+        
+        /*Jorge agrega tu print bajo esta linea*/
+        
+        
+    }
+    
+}
